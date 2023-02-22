fa951a1bd507b372577a22a81d7897eeda4789b2
Lucas Gabriel <lucasgsilva2102@gmail.com>
2023-02-22 10:41:10 -0300

    feat: add rabbitMq Server

--- src/modules/n-queen/n-queen.module.ts
+++ src/modules/n-queen/n-queen.module.ts
@@ -3,10 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { NQueen } from './entities/n-queen.entity';
 import { NQueenService } from './n-queen.service';
 import { NQueenController } from './n-queen.controller';
+import { RabbitMQServer } from '../../shared/infra/rabbitmq-server';
 
 @Module({
   imports: [TypeOrmModule.forFeature([NQueen])],
   controllers: [NQueenController],
-  providers: [NQueenService],
+  providers: [
+    {
+      provide: 'RABBIT_MQ_SERVER',
+      useValue: new RabbitMQServer('amqp://admin:admin@rabbitmq:5672'),
+    },
+    NQueenService,
+  ],
 })
 export class NQueenModule {}
--- src/modules/n-queen/n-queen.service.ts
+++ src/modules/n-queen/n-queen.service.ts
@@ -1,14 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { NQueen } from './entities/n-queen.entity';
 import { ProcessStatus } from '../../shared/enums/process-status.enum';
+import { RabbitMQServer } from '../../shared/infra/rabbitmq-server';
 
 @Injectable()
 export class NQueenService {
   constructor(
     @InjectRepository(NQueen)
     private nQueensRepository: Repository<NQueen>,
+    @Inject('RABBIT_MQ_SERVER')
+    private rabbitMQServer: RabbitMQServer,
   ) {}
 
   async sendToProcess(numberOfQueens: number) {
@@ -18,6 +21,12 @@ export class NQueenService {
     });
 
     const { id } = await this.nQueensRepository.save(newNQueen);
+
+    await this.rabbitMQServer.start();
+    await this.rabbitMQServer.publishInQueue(
+      'n-queen',
+      JSON.stringify({ id, numberOfQueens }),
+    );
 
     return { id };
   }
